8a5d394b9dde3dbccdf44c0586a97d3421fa39cf
holeecow <jasonteo032@gmail.com>
2025-06-27 11:31:27 +0800

    vercel deployment issues

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,7 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
